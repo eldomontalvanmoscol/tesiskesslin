7b7f271643fbb4f816b6d54c3cbcb2bed7935996
amikof <amikof.stone@gmail.com>
2015-03-21 17:16:58 -0500

    index y datagrid

--- _vista/GuiaIngreso.php
+++ _vista/GuiaIngreso.php
@@ -1,8 +1,7 @@
+        <script language="javascript" src="Controles/DataGrid.php"></script>
+
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+?>
 
