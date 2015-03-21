740d5bbf2e1b898c6f1389ba29d14f11630fad41
eldomontalvanmoscol <eldo_1591@hotmail.com>
2015-03-21 15:57:31 -0500

    Login

--- _controlador/LoginC.php
+++ _controlador/LoginC.php
@@ -1,4 +1,26 @@
-<?php
-
-
-
+<div class="main">
+		<div class="login-form">
+			<h1>Member Login</h1>
+					<div class="head">
+						<img src="images/user.png" alt=""/>
+					</div>
+					<form>
+						<input type="text" class="text" value="USERNAME" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'USERNAME';}" >
+						<input type="password" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}">
+						<div class="submit">
+							<input type="submit" onclick="myFunction()" value="LOGIN" >
+					</div>	
+						<p><a href="#">Forgot Password ?</a></p>
+				</form>
+			</div>
+			<!--//End-login-form-->
+			<div class="ad728x90" style="text-align:center">
+				<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+				<!-- w3layouts_demo_728x90 -->
+				<ins class="adsbygoogle"
+				     style="display:inline-block;width:728px;height:90px"
+				     data-ad-client="ca-pub-9153409599391170"
+				     data-ad-slot="8639520288"></ins>
+		   </div>
+		</div>
+			 <!-----//end-main---->
--- index.php
+++ index.php
@@ -3,7 +3,22 @@
         <title>Sistema de Control de Activos</title>
     </head>
     <body>
-       <?php require_once '_controlador/Login.php';?>
+        <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-30027142-1', 'w3layouts.com');
+  ga('send', 'pageview');
+</script>
+<script async src='//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'></script>
+<!-- w3layouts_970x90 -->
+<ins class='adsbygoogle'
+     style='display:block;width:970px;height:90px;margin:0 auto;'
+     data-ad-client='ca-pub-9153409599391170'
+     data-ad-slot='2384823885'></ins>
+
+       <?php include '_controlador/LoginC.php'?>
         
     </body>
 </html>
