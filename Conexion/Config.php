3a6d57a3912b1a03c8565f2f44c595591a9e81e6
eldomontalvanmoscol <eldo_1591@hotmail.com>
2015-03-21 18:52:06 -0500

    sds
    sdsds

--- Conexion/Config.php
+++ Conexion/Config.php
@@ -10,10 +10,5 @@ $usuario_db		="root";
 $password_db	="";
 $bd		="tesiskeslin";
 
-$link = mysqli_connect($servidor,$usuario_db,$password_db,$bd);
-if (!$link) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
-}
 
 ?>
