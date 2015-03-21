1232c5f47262d17b5ada1f9051b1607b16a9b910
eldomontalvanmoscol <eldo_1591@hotmail.com>
2015-03-21 16:05:35 -0500

    Login

--- _controlador/LoginC.php
+++ _controlador/LoginC.php
@@ -5,12 +5,12 @@
 						<img src="images/user.png" alt=""/>
 					</div>
 					<form>
-						<input type="text" class="text" value="USERNAME" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'USERNAME';}" >
-						<input type="password" value="Password" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}">
+						<input type="text" class="text" value="Usuario" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'USERNAME';}" >
+						<input type="password" value="Contraseña" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Password';}">
 						<div class="submit">
 							<input type="submit" onclick="myFunction()" value="LOGIN" >
 					</div>	
-						<p><a href="#">Forgot Password ?</a></p>
+						<p><a href="#">Olvidastes tu contraseña ?</a></p>
 				</form>
 			</div>
 			<!--//End-login-form-->
--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <html>
     <head>
         <title>Sistema de Control de Activos</title>
+        	<meta charset="utf-8">
+		<link href="css/style.css" rel='stylesheet' type='text/css' />
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+		<!--webfonts-->
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:600italic,400,300,600,700' rel='stylesheet' type='text/css'>
+		<!--//webfonts-->
     </head>
     <body>
         <script>
