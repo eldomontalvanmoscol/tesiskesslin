79d728830488101f9a68269b339ac03f1c272f39
eldomontalvanmoscol <eldo_1591@hotmail.com>
2015-03-24 10:48:25 -0500

    Tesis

---
+++ tesis/Conexion/---Config.php
@@ -0,0 +1,7 @@
+<?php 
+define("HOST", "localhost:8080");     // El alojamiento al que deseas conectarte
+define("USER", "root");    // El nombre de usuario de la base de datos
+define("PASSWORD", "");    // La contraseña de la base de datos
+define("DATABASE", "tesiskeslin");
+
+?>
---
+++ tesis/Conexion/Config.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+
+//PARAMETROS DE CONEXION
+
+
+$servidor		="localhost:8080";	
+$usuario_db		="root";
+$password_db	="";
+$bd		="tesiskeslin";
+
+
+?>
